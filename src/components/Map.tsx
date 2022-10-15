f49b85a33383b1466131d01f3ea786bba72df7c4
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-15 22:55:44 +0200

    Change the font size in the popup

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,33 @@
 import mark from "../mark.png";
 import { Icon } from "leaflet";
 import { Marker, Popup } from "react-leaflet";
-import { Grid, Button, Typography } from "@mui/material";
+import {
+    Grid,
+    Button,
+    Typography,
+    createTheme,
+    ThemeProvider,
+} from "@mui/material";
 import { Link } from "react-router-dom";
-import {Crime} from '../features/crimeSlice';
+import { Crime } from "../features/crimeSlice";
 
 const myIcon = new Icon({
     iconUrl: mark,
     iconSize: [32, 32],
+});
+
+const theme = createTheme({
+    typography: {
+        h1: {
+            fontSize: '3rem',
+        },
+        h2: {
+            fontSize: '2rem',
+        },
+        body1: {
+            fontSize: '1rem'
+        }
+    },
 });
 
 interface MapProps {
@@ -17,29 +37,45 @@ interface MapProps {
 
 export function Map({ handleRemove, crime }: MapProps) {
     return (
-        <Marker position={[crime.latitude, crime.longitude] as any as [number, number]} icon={myIcon}>
-            <Popup minWidth={250}>
-                <Typography variant="h4" align="center" sx={{overflowWrap: 'break-word'}}>
-                    {crime.title}
-                </Typography>
-                <Typography variant="body1" sx={{overflowWrap: 'break-word'}}>{crime.description}</Typography>
-                <Grid container justifyContent="space-around">
-                    <Button
-                        component={Link}
-                        to={`editCrime/${crime.id}`}
-                        variant="contained"
-                        sx={{ color: "white!important" }}
+        <ThemeProvider theme={theme}>
+            <Marker
+                position={
+                    [crime.latitude, crime.longitude] as any as [number, number]
+                }
+                icon={myIcon}
+            >
+                <Popup minWidth={250}>
+                    <Typography
+                        variant="h2"
+                        align="center"
+                        sx={{ overflowWrap: "break-word" }}
                     >
-                        Edit
-                    </Button>
-                    <Button
-                        variant="contained"
-                        onClick={() => handleRemove(crime.id)}
+                        {crime.title}
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        sx={{ overflowWrap: "break-word", fontSize: "1.25rem" }}
                     >
-                        Remove
-                    </Button>
-                </Grid>
-            </Popup>
-        </Marker>
+                        {crime.description}
+                    </Typography>
+                    <Grid container justifyContent="space-around">
+                        <Button
+                            component={Link}
+                            to={`editCrime/${crime.id}`}
+                            variant="contained"
+                            sx={{ color: "white!important" }}
+                        >
+                            Edit
+                        </Button>
+                        <Button
+                            variant="contained"
+                            onClick={() => handleRemove(crime.id)}
+                        >
+                            Remove
+                        </Button>
+                    </Grid>
+                </Popup>
+            </Marker>
+        </ThemeProvider>
     );
 }
